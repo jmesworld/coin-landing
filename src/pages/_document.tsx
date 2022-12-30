ae0480ac3407c3b96693e8ff17896fe313551356
Hunter Sides <hunter.s.1337@gmail.com>
2022-12-30 16:44:24 -0600

    fixed auto button highlight in whitespace

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import NextDocument, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-import { getCssText, globalCss } from "../../stitches.config";
+import { getCssText } from "../../stitches.config";
 
 export default class Document extends NextDocument {
   render() {
@@ -18,7 +11,12 @@ export default class Document extends NextDocument {
           <style id="jmes" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
 
-        <body style={{ margin: 0, backgroundColor: "#c7b6fc" }}>
+        <body
+          style={{
+            margin: 0,
+            backgroundColor: "#c7b6fc",
+          }}
+        >
           <Main />
           <NextScript />
         </body>
@@ -26,14 +24,3 @@ export default class Document extends NextDocument {
     );
   }
 }
-
-// class Document extends NextDocument {
-//   static async getInitialProps(
-//     ctx: DocumentContext
-//   ): Promise<DocumentInitialProps> {
-//     const initialProps = await Document.getInitialProps(ctx);
-
-//     return initialProps;
-//   }
-// }
-// export default Document;
